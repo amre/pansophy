783293a61eb2cc148292582c83a00926f47119d5
Trisha Fultz <tfultz12@wooster.edu>
2011-06-09 23:27:44 -0400

    Changed description for user that cannot be deleted

--- Pansophy/interface/deleteuser.php
+++ Pansophy/interface/deleteuser.php
@@ -33,7 +33,7 @@ else if(strcmp($_POST['submit'], 'No') == 0){
 	echo '<meta http-equiv="Refresh" content="0; URL=./viewuser.php?id='.$ID.'">';
 }
 else if($dam->getUserIssues($ID,0) || $dam->getUserContacts($ID,0)){
-	echo "<br>$FirstName $LastName has created issues and/or contacts that are still in the system. <br>You can either delete the associated issues and contacts, <br>or you can set his/her access level to \"No Access\" using the \"Edit this user\" link.";
+	echo "<br>$FirstName $LastName has created issues and/or contacts that are still in the system. <br>You can either delete the associated issues and contacts and then delete the user, <br>or you can just set his/her access level to \"No Access\" using the \"Edit this user\" link.";
 }
 else{
 	echo "<br>Do you really want to delete $FirstName $LastName from the system?";
